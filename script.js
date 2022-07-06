008e938846136bbe75d663093879d765d0c4a8e5
jmcushnie <94955791+jmcushnie@users.noreply.github.com>
2022-07-06 22:42:35 +1200

    fix: image size and cleaned up code
    Resized image to that was too large for the site. Used Prettier extension to clean up code.

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 // hamburger menu
 
-let hamburger = document.querySelector('.hamburger')
-let navMenu = document.querySelector('.nav-menu')
+let hamburger = document.querySelector(".hamburger");
+let navMenu = document.querySelector(".nav-menu");
 
 hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-})
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
 
-document.querySelector(".nav-item").forEach(n =>
-    n.addEventListener("click", () => {
-        hamburger.classList.remove("active");
-        navMenu.classList.remove("active");
-    }))
+document.querySelector(".nav-item").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  })
+);
